e7c6b8cc0e9b9e2d8ee845c9abdd9cc7550862e6
hanasayyida <hanasayyida2711@gmail.com>
2023-05-16 17:15:20 +0800

    Profil
    16 Mei 2023

--- resources/views/landingpage/formasipekerjaan.blade.php
+++ resources/views/landingpage/formasipekerjaan.blade.php
@@ -2,88 +2,110 @@
 
 @section('content')
 
-      <!-- ======= Testimonials Section ======= -->
-      <section id="testimonials" class="testimonials">
-        <div class="container">
-  
-          <div class="section-title" data-aos="fade-up">
-            <h2>Testimonials</h2>
-            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-          </div>
-  
-          <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
-            <div class="swiper-wrapper">
-  
-              <div class="swiper-slide">
-                <div class="testimonial-item">
-                  <p>
-                    <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                    Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
-                    <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                  </p>
-                  <img src="{{ asset('LandingPage-Template/assets/img/testimonials/testimonials-1.jpg') }}" class="testimonial-img" alt="">
-                  <h3>Saul Goodman</h3>
-                  <h4>Ceo &amp; Founder</h4>
-                </div>
-              </div><!-- End testimonial item -->
-  
-              <div class="swiper-slide">
-                <div class="testimonial-item">
-                  <p>
-                    <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                    Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-                    <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                  </p>
-                  <img src="{{ asset('LandingPage-Template/assets/img/testimonials/testimonials-2.jpg') }}" class="testimonial-img" alt="">
-                  <h3>Sara Wilsson</h3>
-                  <h4>Designer</h4>
-                </div>
-              </div><!-- End testimonial item -->
-  
-              <div class="swiper-slide">
-                <div class="testimonial-item">
-                  <p>
-                    <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                    Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
-                    <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                  </p>
-                  <img src="{{ asset('LandingPage-Template/assets/img/testimonials/testimonials-3.jpg') }}" class="testimonial-img" alt="">
-                  <h3>Jena Karlis</h3>
-                  <h4>Store Owner</h4>
-                </div>
-              </div><!-- End testimonial item -->
-  
-              <div class="swiper-slide">
-                <div class="testimonial-item">
-                  <p>
-                    <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                    Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
-                    <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                  </p>
-                  <img src="{{ asset('LandingPage-Template/assets/img/testimonials/testimonials-4.jpg') }}" class="testimonial-img" alt="">
-                  <h3>Matt Brandon</h3>
-                  <h4>Freelancer</h4>
-                </div>
-              </div><!-- End testimonial item -->
-  
-              <div class="swiper-slide">
-                <div class="testimonial-item">
-                  <p>
-                    <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                    Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-                    <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                  </p>
-                  <img src="{{ asset('LandingPage-Template/assets/img/testimonials/testimonials-5.jpg') }}" class="testimonial-img" alt="">
-                  <h3>John Larson</h3>
-                  <h4>Entrepreneur</h4>
-                </div>
-              </div><!-- End testimonial item -->
-  
-            </div>
-            <div class="swiper-pagination"></div>
-          </div>
-  
+     <!-- ======= F.A.Q Section ======= -->
+     <section id="faq" class="faq">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Formasi Pekerjaan</h2>
         </div>
-      </section><!-- End Testimonials Section -->
+
+        <ul class="faq-list">
+
+          <li>
+            <div data-bs-toggle="collapse" class="collapsed question" href="#faq1">Satpam<i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
+            <div id="faq1" class="collapse" data-bs-parent=".faq-list">
+              <ul>
+                <i></i><p>1. Pendidikan Formal (Minimal SLTA)</p>
+                <i></i><p>2. Pendidikan Non Formal (Jika ada)</p>
+                <i></i><p>3. Usia minimal 18 tahun & maksimal 50 tahun</p>
+                <i></i><p>4. Pengalaman kerja sebagai Satpam (Diutamakan)</p>
+                <i></i><p>5. Pendidikan Satpam (Diutamakan)</p>
+                <i></i><p>6. Tinggi badan minimal 160 cm untuk wanita & 165 untuk pria</p>
+              </ul>
+            </div>
+          </li>
+
+          <li>
+            <div data-bs-toggle="collapse" href="#faq2" class="collapsed question">Cleaning Service<i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
+            <div id="faq2" class="collapse" data-bs-parent=".faq-list">
+              <ul>
+                <i></i><p>1. Pendidikan Formal (Minimal SLTA)</p>
+                <i></i><p>2. Pendidikan Non Formal (Jika ada)</p>
+                <i></i><p>3. Usia minimal 20 tahun & maksimal 40 tahun</p>
+                <i></i><p>4. Pengalaman kerja sebagai Cleaning Service (Diutamakan)</p>
+              </ul>
+            </div>
+          </li>
+
+          <li>
+            <div data-bs-toggle="collapse" href="#faq3" class="collapsed question">Teknisi<i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
+            <div id="faq3" class="collapse" data-bs-parent=".faq-list">
+              <ul>
+                <i></i><p>1. Pendidikan Formal (Minimal D3)</p>
+                <i></i><p>2. Pendidikan Non Formal (Jika ada)</p>
+                <i></i><p>3. Usia minimal 22 tahun & maksimal 35 tahun</p>
+                <i></i><p>4. Pengalaman kerja sebagai Teknisi (Diutamakan)</p>
+                <i></i><p>5. Mampu mengoperasikan komputer (MS Office)</p>
+              </ul>
+            </div>
+          </li>
+
+          <li>
+            <div data-bs-toggle="collapse" href="#faq4" class="collapsed question">Helper<i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
+            <div id="faq4" class="collapse" data-bs-parent=".faq-list">
+              <ul>
+                <i></i><p>1. Pendidikan Formal (Minimal D3)</p>
+                <i></i><p>2. Pendidikan Non Formal (Jika ada)</p>
+                <i></i><p>3. Usia minimal 22 tahun & maksimal 35 tahun</p>
+                <i></i><p>4. Pengalaman kerja sebagai Helper (Diutamakan)</p>
+                <i></i><p>5. Mampu mengoperasikan komputer (MS Office)</p>
+              </ul>
+            </div>
+          </li>
+
+          <li>
+            <div data-bs-toggle="collapse" href="#faq5" class="collapsed question">Operator<i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
+            <div id="faq5" class="collapse" data-bs-parent=".faq-list">
+              <ul>
+                <i></i><p>1. Pendidikan Formal (Minimal D3)</p>
+                <i></i><p>2. Pendidikan Non Formal (Jika ada)</p>
+                <i></i><p>3. Usia minimal 24 tahun & maksimal 35 tahun</p>
+                <i></i><p>4. Pengalaman kerja sebagai Operator (Diutamakan)</p>
+                <i></i><p>5. Mampu mengoperasikan komputer (MS Office)</p>
+              </ul>
+            </div>
+          </li>
+
+          <li>
+            <div data-bs-toggle="collapse" href="#faq6" class="collapsed question">Admin<i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
+            <div id="faq6" class="collapse" data-bs-parent=".faq-list">
+              <ul>
+                <i></i><p>1. Pendidikan Formal (Minimal D3)</p>
+                <i></i><p>2. Pendidikan Non Formal (Jika ada)</p>
+                <i></i><p>3. Usia minimal 20 tahun & maksimal 35 tahun</p>
+                <i></i><p>4. Pengalaman kerja sebagai Admin (Diutamakan)</p>
+                <i></i><p>5. Mampu mengoperasikan komputer (MS Office)</p>
+              </ul>
+            </div>
+          </li>
+
+          <li>
+            <div data-bs-toggle="collapse" href="#faq7" class="collapsed question">Driver<i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
+            <div id="faq7" class="collapse" data-bs-parent=".faq-list">
+              <ul>
+                <i></i><p>1. Pendidikan Formal (Minimal SLTA)</p>
+                <i></i><p>2. Pendidikan Non Formal (Jika ada)</p>
+                <i></i><p>3. Usia minimal 24 tahun & maksimal 40 tahun</p>
+                <i></i><p>4. Pengalaman kerja sebagai Driver (Diutamakan)</p>
+                <i></i><p>5. Memiliki Surat Izin Mengemudi (SIM)</p>
+              </ul>
+            </div>
+          </li>
+
+        </ul>
+
+      </div>
+    </section><!-- End Frequently Asked Questions Section -->
 
 @endsection
--- resources/views/landingpage/profilperusahaan.blade.php
+++ resources/views/landingpage/profilperusahaan.blade.php
@@ -19,6 +19,15 @@
                 <p>
                   PT Yaza Pratama berdiri sejak tahun 2003 dan telah lebih 18 tahun berkarya dan berperan aktif dalam Penyediaan Jasa Tenaga Alih Daya diberbagai Instansi, baik Pemerintah, BUMN, Swasta maupun Perorangan. Banyak pengalaman dan proses pembelajaran yang telah dilewati dari tahun ke tahun yang membawa perusahaan terus tumbuh dan berkembang sesuai dengan rencana strategis perusahaan.
                 </p>
+                <p>
+                  Banyak pengalaman dan proses pembelajaran yang telah dilewati dari tahun ke tahun yang membawa perusahaan terus tumbuh dan berkembang sesuai dengan rencana strategis perusahaan.
+                </p>
+                <p>
+                  Penyediaan Jasa Tenaga Alih Daya hanya dapat dicapai melalui komitmen dan dukungan seluruh mitra perusahaan serta penerapan manajemen yang efektif dan efisien yang berkesinambungan dengan memperhatikan aspek Keselamatan dan Kesehatan Kerja (K3) di lingkungan kerja.
+                </p>
+                <p>
+                  Sebagai wujud dari rangkaian pembelajaran dan pengalaman perusahaan PT Yaza Pratama siap memberikan Jasa dan Pelayanan Tenaga Alih Daya yang terbaik bagi pengguna. 
+                </p>
               </div>
             </div>
           </div>
@@ -73,4 +82,65 @@
         </div>
       </section><!-- End Features Section -->
 
+      <!-- ======= Team Section ======= -->
+      <section id="team" class="team">
+        <div class="container">
+  
+          <div class="section-title" data-aos="fade-up">
+            <h2>Susunan Direksi</h2>
+            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          </div>
+  
+          <div class="row">
+  
+            <div class="col-lg-4 col-md-6">
+              <div class="member" data-aos="zoom-in">
+                <div class="pic"><img src="{{ asset('LandingPage-Template/assets/img/team/team-1.jpg') }}" class="img-fluid" alt=""></div>
+                <div class="member-info">
+                  <h4>Walter White</h4>
+                  <span>Chief Executive Officer</span>
+                  <div class="social">
+                    <a href=""><i class="bi bi-facebook"></i></a>
+                    <a href=""><i class="bi bi-instagram"></i></a>
+                    <a href=""><i class="bi bi-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-lg-4 col-md-6">
+              <div class="member" data-aos="zoom-in" data-aos-delay="100">
+                <div class="pic"><img src="{{ asset('LandingPage-Template/assets/img/team/team-2.jpg') }}" class="img-fluid" alt=""></div>
+                <div class="member-info">
+                  <h4>Kumala Sari</h4>
+                  <span>Direktur</span>
+                  <div class="social">
+                    <a href=""><i class="bi bi-facebook"></i></a>
+                    <a href="https://www.instagram.com/yaza_pratama/"><i class="bi bi-instagram"></i></a>
+                    <a href=""><i class="bi bi-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-lg-4 col-md-6">
+              <div class="member" data-aos="zoom-in" data-aos-delay="200">
+                <div class="pic"><img src="{{ asset('LandingPage-Template/assets/img/team/team-3.jpg') }}" class="img-fluid" alt=""></div>
+                <div class="member-info">
+                  <h4>William Anderson</h4>
+                  <span>CTO</span>
+                  <div class="social">
+                    <a href=""><i class="bi bi-facebook"></i></a>
+                    <a href="https://www.instagram.com/yaza_pratama/"><i class="bi bi-instagram"></i></a>
+                    <a href=""><i class="bi bi-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+  
+          </div>
+  
+        </div>
+      </section><!-- End Team Section -->
+      
 @endsection
